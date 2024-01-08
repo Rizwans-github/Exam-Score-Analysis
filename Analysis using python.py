092565884d5dc881188a84d78cbe0046c08fa42d
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-01-08 15:21:24 +0530

    Using python

---
+++ Analysis using python.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv('C:/Users/Mohd Rizwan/Downloads/Da Project/Exam Score Analysis using Python/Expanded_data_with_more_features.csv')
+df.head()
+df.describe()
+df.info()
